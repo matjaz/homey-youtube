e3c2b23f5dfd46fe0ba18668958aa5be2f08853f
Matjaz Lipus <matjazl@gmail.com>
2016-02-24 20:48:01 +0100

    keep it simple

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 'use strict'
 
-var onFlowActionSearch = require('./lib/search').onFlowActionSearch
-var onFlowActionSearchLive = require('./lib/search').onFlowActionSearchLive
-var onFlowActionPlayListItems = require('./lib/playlist').onFlowActionPlayListItems
-var onFlowActionSearchAutocomplete = require('./lib/search').onFlowActionSearchAutocomplete
+var search = require('./lib/search')
+var playlist = require('./lib/playlist')
 
 exports.init = function () {
-  // Homey.log('Youtube')
 
-  Homey.manager('flow').on('action.searchYoutube', onFlowActionSearch)
-  Homey.manager('flow').on('action.searchYoutubeLive', onFlowActionSearchLive)
-  Homey.manager('flow').on('action.getYoutubePlaylistURL', onFlowActionPlayListItems)
-  Homey.manager('flow').on('action.searchYoutube.source.autocomplete', onFlowActionSearchAutocomplete)
+  search.init()
+  playlist.init()
+
+  Homey.log('Youtube ready')
 }
