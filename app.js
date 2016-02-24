f15767b34148e0f4206bb82f8c4733290db94661
Matjaz Lipus <matjazl@gmail.com>
2016-02-24 20:59:22 +0100

    speech support

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 'use strict'
 
 var media = require('./lib/media')
+var speech = require('./lib/speech-input')
 var search = require('./lib/search')
 var playlist = require('./lib/playlist')
 
 exports.init = function () {
   media.init()
+  speech.init()
 
   search.init()
   playlist.init()
